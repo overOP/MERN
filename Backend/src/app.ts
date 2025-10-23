9d5eac3348611760dfc7b2254c7a74d02e9ad2f8
overOP <cgpradeepop@gmail.com>
2025-10-23 19:27:32 +0545

    fix app.js and make model of user

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,14 +1,16 @@
-import express, { type Application, type Request, type Response } from 'express';
-import './database/connection';
 import * as dotenv from 'dotenv'
 dotenv.config()
+
+import express, { type Application, type Request, type Response } from 'express';
+import './database/connection';
 const app: Application = express();
+
 const port: string = process.env.PORT!;
 
 app.get("/", (req: Request, res: Response) => {
   res.send(`<h1>🚀 Server is running 🚀</h1>`);
 });
 
-app.listen(port, () => {
+app.listen(Number(port), () => {
   console.log(`🚀 Server is running on http://localhost:${port}`);
 });
---
+++ Backend/src/database/models/userModel.ts
@@ -0,0 +1,40 @@
+import {
+    Table,
+    Column,
+    Model,
+    DataType,
+ } from 'sequelize-typescript'
+
+ @Table({
+    tableName: 'users', // specify the table name
+    modelName: 'User', // specify the model name
+    timestamps: true, // enable timestamps
+ })
+
+ //extends is used to inherit properties and methods from the Model class
+ class User extends Model {
+    @Column({
+        primaryKey: true,
+        type: DataType.UUID, //UUID type for unique identifier
+        defaultValue: DataType.UUIDV4, //automatically generate UUID v4
+    })
+    declare id: string;
+
+    @Column({
+        type: DataType.STRING,
+    })
+    declare username: string;
+
+    @Column({
+        type: DataType.STRING,
+        unique: true,
+    })
+    declare email: string;
+
+    @Column({
+        type: DataType.STRING,
+    })
+    declare password: string;
+ }
+
+ export default User;

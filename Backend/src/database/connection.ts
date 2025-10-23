3c5ae99687bc158f50619ca902202f324afd4c17
overOP <cgpradeepop@gmail.com>
2025-10-23 17:41:48 +0545

    modify database connection

---
+++ Backend/src/database/connection.ts
@@ -0,0 +1,29 @@
+import { Sequelize } from "sequelize-typescript";
+
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME!,
+  dialect: "mysql",
+  username: process.env.DB_USERNAME!,
+  password: process.env.DB_PASSWORD!,
+  host: process.env.DB_HOST!,
+  port: Number(process.env.DB_PORT!),
+  models: [__dirname + "/models"], // path to your models
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log("✅ Database connected successfully.");
+  })
+  .catch((err: Error) => {
+    console.error("❌ Unable to connect to the database:", err);
+  });
+
+sequelize.sync()
+  .then(() => {
+    console.log("✅ All models were synchronized successfully.");
+  })
+  .catch((err: Error) => {
+    console.error("❌ An error occurred while synchronizing the models:", err);
+  });
+
+export default sequelize;

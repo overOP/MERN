c6df03aa9417d4740dd5c1d32351f0277e258914
overOP <cgpradeepop@gmail.com>
2025-11-13 16:01:58 +0545

    make all order

--- src/controllers/order/orderController.ts
+++ src/controllers/order/orderController.ts
@@ -1,10 +1,25 @@
-import { Response } from "express";
+import { Response, Request } from "express";
 import { AuthRequest } from "../../middleware/auth";
-import { KhaltiResponse, OrderData, PaymentMethod } from "../../types/orderTypes";
+import {
+  KhaltiResponse,
+  OrderData,
+  OrderStatus,
+  PaymentMethod,
+  PaymentStatus,
+  TransactionStatus,
+  TransactionVerificationResponse,
+} from "../../types/orderTypes";
 import Order from "../../database/models/orderModel";
 import Payment from "../../database/models/paymentModel";
 import OrderDetail from "../../database/models/orderDetailsModel";
 import axios from "axios";
+import Product from "../../database/models/productModel";
+import Category from "../../database/models/categoryModel";
+import User from "../../database/models/userModel";
+
+class ExtendedOrder extends Order{
+  declare paymentId : string | null
+}
 
 class OrderController {
   async createOrder(req: AuthRequest, res: Response): Promise<void> {
@@ -45,7 +60,7 @@ class OrderController {
       paymentId: Data.id,
     });
 
-    let responseOrderData ; 
+    let responseOrderData;
 
     for (let i = 0; i < items.length; i++) {
       await OrderDetail.create({
@@ -58,31 +73,203 @@ class OrderController {
     if (paymentDetails.paymentMethod === PaymentMethod.Khalti) {
       // Khalti integration
       const paymentData = {
-          return_url: "http://localhost:5050/success", // frontend url
-          purchase_order_id: orderData.id,
-          amount: totalAmount * 100,  // amount in paisa
-          website_url: process.env.BASE_URL, // frontend url
-          purchase_order_name : 'orderName_' + orderData.id,
-      }
-      const response = await axios.post('https://dev.khalti.com/api/v2/epayment/initiate/', paymentData, {
+        return_url: "http://localhost:5050/success", // frontend url
+        purchase_order_id: orderData.id,
+        amount: totalAmount * 100, // amount in paisa
+        website_url: process.env.BASE_URL, // frontend url
+        purchase_order_name: "orderName_" + orderData.id,
+      };
+      const response = await axios.post(
+        "https://dev.khalti.com/api/v2/epayment/initiate/",
+        paymentData,
+        {
           headers: {
-              'Authorization': `Key ${process.env.KHALTI_SECRET_KEY}`
-          }
-      })
-      const KhaltiResponse : KhaltiResponse = response.data
-      Data.pidx = KhaltiResponse.pidx
-      Data.save()
+            Authorization: `Key ${process.env.KHALTI_SECRET_KEY}`,
+          },
+        }
+      );
+      const KhaltiResponse: KhaltiResponse = response.data;
+      Data.pidx = KhaltiResponse.pidx;
+      Data.save();
       res.status(200).json({
         message: "Order placed successfully",
         rel: KhaltiResponse.payment_url,
         data: responseOrderData,
-      })
+      });
     } else {
       res.status(200).json({
         message: "Order placed successfully",
         data: orderData,
       });
     }
+  }
+  async verifyPayment(req: AuthRequest, res: Response): Promise<void> {
+    const { pidx } = req.body;
+    if (!pidx) {res.status(404).json({
+        message: "Missing required field(s): pidx",
+      });
+      return;
+    }
+    const response = await axios.post("https://a.khalti.com/api/v2/epayment/lookup/",{ pidx },{
+        headers: {
+          Authorization: `Key ${process.env.KHALTI_SECRET_KEY}`,
+        },
+      }
+    );
+    const data: TransactionVerificationResponse = response.data;
+    if (data.status === TransactionStatus.Completed) {
+      await Payment.update(
+        { paymentStatus: "paid" },
+        { where: { pidx: pidx } }
+      );
+      res.status(200).json({
+        message: "Payment verified successfully",
+        data: data.status + " " + data.total_amount,
+      });
+    } else {
+      res.status(400).json({
+        message: "Payment not verified",
+      });
+    }
+  }
+  // customer SIDE Starts here
+  async fetchMyOrders(req: AuthRequest, res: Response): Promise<void> {
+    const userId = req.user?.id;
+    const orders = await Order.findAll({
+      where: { userId },
+      include: [
+        {
+          model: Payment,
+        },
+      ],
+    });
+    if (!orders) {
+      res.status(404).json({
+        message: "Orders not found",
+        data: [],
+      });
+      return;
+    }
+    res.status(200).json({
+      message: "Orders found successfully",
+      data: orders,
+    });
+  }
+  async fetchOrderDetails(req: AuthRequest, res: Response): Promise<void> {
+    const orderId = req.params.id;
+    const orderDetails = await OrderDetail.findAll({
+      where: { orderId },
+      include: [
+        {
+          model: Product,
+          include: [
+            {
+              model: Category,
+              attributes: ["categoryName"],
+            },
+          ],
+        },
+        {
+          model: Order,
+          include: [
+            {
+              model: Payment,
+              attributes: ["paymentMethod", "paymentStatus"],
+            },
+            {
+              model: User,
+              attributes: ["username", "email"],
+            },
+          ],
+        },
+      ],
+    });
+    if (orderDetails.length == 0) {
+      res.status(404).json({
+        message: "Order details not found",
+        data: [],
+      });
+      return;
+    }
+    res.status(200).json({
+      message: "Order details found successfully",
+      data: orderDetails,
+    });
+  }
+  async cancelMyOrder(req: AuthRequest, res: Response): Promise<void> {
+    const userId = req.user?.id;
+    const orderId = req.params.id;
+
+    const order: any = await Order.findAll({ where: { userId, id: orderId } });
+    if (
+      order?.orderStatus === OrderStatus.Ontheway ||
+      order?.orderStatus === OrderStatus.Preparation
+    ) {
+      res.status(200).json({
+        message: "You cannot cancell order when it is in ontheway or prepared",
+      });
+      return;
+    }
+    await Order.update(
+      { orderStatus: OrderStatus.Cancelled },
+      { where: { id: orderId } }
+    );
+    res.status(200).json({
+      message: "Order cancelled successfully",
+    });
+  }
+
+  // Admin side starts here
+  async changeOrderStatus(req: Request, res: Response): Promise<void> {
+    const orderId = req.params.id;
+    const orderStatus : OrderStatus = req.body.orderStatus;
+    
+    await Order.update({ orderStatus : orderStatus }, { where : { id : orderId }});
+    res.status(200).json({
+      message: "Order status changed successfully",
+    });
+  }
+  async changePaymentStatus(req: Request, res: Response): Promise<void> {
+    const orderId = req.params.id;
+    const paymentStatus : PaymentStatus = req.body.paymentStatus;
+    const order = await Order.findByPk(orderId);
+    const extendedOrder : ExtendedOrder = order as ExtendedOrder
+    await Payment.update({ paymentStatus : paymentStatus }, { where : { id : extendedOrder.paymentId }});
+    res.status(200).json({
+      message: "Payment status changed successfully",
+    });
+  }
+  async deleteOrder(req: Request, res: Response): Promise<void> {
+    const orderId = req.params.id
+    const order = await Order.findByPk(orderId);
+    const extendedOrder : ExtendedOrder = order as ExtendedOrder
+
+    if(!order){
+      res.status(404).json({
+        message: "Order not found",
+      });
+      return
+    }
+    await OrderDetail.destroy({ where : { orderId : orderId }});
+    await Payment.destroy({ where : { id : extendedOrder.paymentId }});
+    await Order.destroy({ where : { id : orderId }});
+    res.status(200).json({
+      message: "Order deleted successfully",
+    });
+  }
+  async fetchOrders(req: AuthRequest, res: Response): Promise<void> {
+    const orders = await Order.findAll({ include : [{ model : Payment }]});
+    if (!orders) {
+      res.status(404).json({
+        message: "Orders not found",
+        data: [],
+      });
+      return;
+    }
+    res.status(200).json({
+      message: "Orders found successfully",
+      data: orders,
+    });
   }
 }
 
--- src/routes/order/orderRoute.ts
+++ src/routes/order/orderRoute.ts
@@ -1,10 +1,24 @@
 import express, { Router } from "express";
-import auth from "../../middleware/auth";
+import auth, { Role } from "../../middleware/auth";
 import catchAsync from "../../utils/catchAsync";
 import orderController from "../../controllers/order/orderController";
 const router: Router = express.Router();
 
 router.route("/order")
+  .get(auth.isAuthenticated, catchAsync(orderController.fetchOrders))
   .post(auth.isAuthenticated, catchAsync(orderController.createOrder));
+router.route("/order/verify")
+  .post(auth.isAuthenticated,auth.restrictTo(Role.Admin), catchAsync(orderController.verifyPayment));
 
+router.route("/customer")
+  .get(auth.isAuthenticated, catchAsync(orderController.fetchMyOrders));
+router.route("/customer/:id")
+  .post(auth.isAuthenticated,auth.restrictTo(Role.Customer), catchAsync(orderController.cancelMyOrder))
+  .get(auth.isAuthenticated,auth.restrictTo(Role.Customer), catchAsync(orderController.fetchOrderDetails))
+
+router.route("/admin/payment/:id")
+  .patch(auth.isAuthenticated,auth.restrictTo(Role.Admin), catchAsync(orderController.changePaymentStatus))
+router.route("/admin/order/:id")
+  .patch(auth.isAuthenticated,auth.restrictTo(Role.Admin), catchAsync(orderController.changeOrderStatus))
+  .delete(auth.isAuthenticated,auth.restrictTo(Role.Admin), catchAsync(orderController.deleteOrder))  
 export default router;
